216f1329abd1f46e09cdee3e4e49ce4b976e9b29
Patxi Bocos <patxi.bocos@revolut.com>
2025-03-30 16:30:55 +0200

    refactor: Use injected CoroutineScope in MyCyclistFirebaseMessagingService for better testability

--- composeApp/src/androidMain/kotlin/io/github/patxibocos/mycyclist/data/gzip/MyCyclistFirebaseMessagingService.kt
+++ composeApp/src/androidMain/kotlin/io/github/patxibocos/mycyclist/data/gzip/MyCyclistFirebaseMessagingService.kt
@@ -6,6 +6,7 @@ import com.google.firebase.messaging.RemoteMessage
 import io.github.patxibocos.mycyclist.domain.repository.DataRepository
 import io.github.patxibocos.mycyclist.domain.repository.firebaseDataRepository
 import io.github.patxibocos.mycyclist.ui.notification.NotificationBuilder
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 
@@ -13,10 +14,11 @@ import kotlinx.coroutines.launch
 internal class MyCyclistFirebaseMessagingService(
     private val dataRepository: DataRepository = firebaseDataRepository,
     private val notificationBuilder: NotificationBuilder = NotificationBuilder(),
+    private val mainScope: CoroutineScope = MainScope(),
 ) : FirebaseMessagingService() {
 
     override fun onMessageReceived(message: RemoteMessage) {
-        MainScope().launch {
+        mainScope.launch {
             if (!dataRepository.refresh()) {
                 // If data fails to refresh, we skip the rest
                 return@launch
