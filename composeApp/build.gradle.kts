000aa35ecd7cebc44299390a4e825071542b2f40
Patxi Bocos <patxi952@gmail.com>
2025-06-18 19:07:22 +0000

    Use ktor client android

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -47,7 +47,7 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
-            implementation(libs.ktor.client.okhttp)
+            implementation(libs.ktor.client.android)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
